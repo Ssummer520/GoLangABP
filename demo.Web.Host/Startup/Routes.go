2f85b724e4b72de0ba37b14f39e9c458d031072f
MasterAlan <1362246612@qq.com>
2022-04-15 20:24:17 +0800

    调整代码目录

--- demo.Web.Host/Startup/Routes.go
+++ demo.Web.Host/Startup/Routes.go
@@ -1,17 +1,11 @@
 package Startup
 
-import (
-	"GoLangABP/demo.Web.Host/Authentication"
-	. "GoLangABP/demo.Web.Host/controllers"
-	"github.com/gin-gonic/gin"
-)
-
 //路由
-var IndexC Index
-var UserC UserLogin
-
-func ConfigureRoute(r *gin.Engine) {
-	//controller declare
-
-	r.Use(Authentication.JwtVerify)
-}
+//var IndexC Index
+//var UserC UserLogin
+//
+//func ConfigureRoute(r *gin.Engine) {
+//	//controller declare
+//
+//	r.Use(Authentication.JwtVerify)
+//}
