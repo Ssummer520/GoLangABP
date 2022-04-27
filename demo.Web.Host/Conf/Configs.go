aea172e62b56b28f1bd5a062fa9796c3e2544d86
13622 <1362246612@qq.com>
2022-04-27 23:59:36 +0800

    修改sqlx 兼容问题

--- demo.Web.Host/Conf/Configs.go
+++ demo.Web.Host/Conf/Configs.go
@@ -75,6 +75,5 @@ func GetAppPath() string {
 	file, _ := exec.LookPath(os.Args[0])
 	path, _ := filepath.Abs(file)
 	index := strings.LastIndex(path, string(os.PathSeparator))
-
 	return path[:index]
 }
